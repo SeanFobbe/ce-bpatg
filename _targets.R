1c37a76557cb1a583f160451f4627c90ffbb2564
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-10 11:51:06 +0200

    add packages

--- _targets.R
+++ _targets.R
@@ -13,8 +13,23 @@ summ <- function(dataset) {
   summarize(dataset, mean_x = mean(x))
 }
 
-# Set target-specific options such as packages.
-tar_option_set(packages = "dplyr")
+## Set target-specific options such as packages.
+
+tar_option_set(packages = "fs",           # Verbessertes File Handling
+               "mgsub",        # Vektorisiertes Gsub
+               "httr",         # HTTP-Werkzeuge
+               "rvest",        # HTML/XML-Extraktion
+               "knitr",        # Professionelles Reporting
+               "kableExtra",   # Verbesserte Kable Tabellen
+               "pdftools",     # Verarbeitung von PDF-Dateien
+               "doParallel",   # Parallelisierung
+               "ggplot2",      # Fortgeschrittene Datenvisualisierung
+               "scales",       # Skalierung von Diagrammen
+               "data.table",   # Fortgeschrittene Datenverarbeitung
+               "readtext",     # TXT-Dateien einlesen
+               "quanteda")     # Fortgeschrittene Computerlinguistik
+
+
 
 # End this file with a list of target objects.
 list(
