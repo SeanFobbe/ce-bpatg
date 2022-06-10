19ad47c742917a7d6311cdad36cf2095fffaa256
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-10 11:51:49 +0200

    add targets preamble

--- _targets.R
+++ _targets.R
@@ -1,19 +1,22 @@
 library(targets)
-# This is an example _targets.R file. Every
-# {targets} pipeline needs one.
-# Use tar_script() to create _targets.R and tar_edit()
-# to open it again for editing.
-# Then, run tar_make() to run the pipeline
-# and tar_read(summary) to view the results.
+library(tarchetypes)
+
 
 # Define custom functions and other global objects.
 # This is where you write source(\"R/functions.R\")
 # if you keep your functions in external scripts.
-summ <- function(dataset) {
-  summarize(dataset, mean_x = mean(x))
-}
+
+source("R-fobbe-proto-package/f.linkextract.R")
+source("R-fobbe-proto-package/f.year.iso.R")
+source("R-fobbe-proto-package/f.dopar.pdfextract.R")
+source("R-fobbe-proto-package/f.hyphen.remove.R")
+source("R-fobbe-proto-package/f.fast.freqtable.R")
+source("R-fobbe-proto-package/f.lingsummarize.iterator.R")
+source("R-fobbe-proto-package/f.dopar.multihashes.R")
+source("R-fobbe-proto-package/f.dopar.spacyparse.R")
 
 ## Set target-specific options such as packages.
+
 
 tar_option_set(packages = "fs",           # Verbessertes File Handling
                "mgsub",        # Vektorisiertes Gsub
