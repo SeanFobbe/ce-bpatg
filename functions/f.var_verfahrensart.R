ca79c4be2562b3a5d6e56f5f9a57debd135562e9
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-19 02:57:56 +0200

    add comma

--- functions/f.var_verfahrensart.R
+++ functions/f.var_verfahrensart.R
@@ -1,7 +1,8 @@
 
 
-f.var_verfahrensart <- function(x
-                                az.brd){
+f.var_verfahrensart <- function(x,
+                                az.brd,
+                                gericht){
 
     ## Die Registerzeichen werden an dieser Stelle mit ihren detaillierten Bedeutungen aus dem folgenden Datensatz abgeglichen: "Seán Fobbe (2021). Aktenzeichen der Bundesrepublik Deutschland (AZ-BRD). Version 1.0.1. Zenodo. DOI: 10.5281/zenodo.4569564." Das Ergebnis des Abgleichs wird in der Variable "verfahrensart" in den Datensatz eingefügt.
 
@@ -10,15 +11,15 @@ f.var_verfahrensart <- function(x
 
 
     ## Datensatz auf relevante Daten reduzieren
-    az.bpatg <- az.source[stelle == "BPatG" & position == "hauptzeichen"]
+    az.gericht <- az.source[stelle == gericht & position == "hauptzeichen"]
     
     
     ## Indizes bestimmen
     targetindices <- match(x$registerzeichen,
-                           az.bpatg$zeichen_code)
+                           az.gericht$zeichen_code)
     
     ## Vektor der Verfahrensarten erstellen und einfügen
-    verfahrensart <- az.bpatg$bedeutung[targetindices]
+    verfahrensart <- az.gericht$bedeutung[targetindices]
     
     
     return(verfahrensart)
