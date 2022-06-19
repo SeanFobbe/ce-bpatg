26bb12e03be8f9185bc0364c72305cc0eb297667
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-19 03:08:02 +0200

    add argument to var verfahrensart

--- _targets.R
+++ _targets.R
@@ -184,7 +184,9 @@ tar.enhance <- list(tar_target(dt.bpatg.datecleaned,
                                lapply(dt.bpatg.datecleaned$text,
                                       f.hyphen.remove)),
                     tar_target(var_verfahrensart,
-                               f.var_verfahrensart(dt.bpatg.cleaned$registerzeichen))
+                               f.var_verfahrensart(dt.bpatg.cleaned$registerzeichen,
+                                                   az.brd = az.brd,
+                                                   gericht = "BPatG"))
                     )
 
 
