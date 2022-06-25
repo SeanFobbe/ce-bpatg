60168c076083eda4b4c578bba02e51f2e27423d9
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-25 03:16:06 +0200

    init final function

---
+++ functions/f.dataset_finalize.R
@@ -0,0 +1,21 @@
+
+
+
+f.dataset_finalize <- function(x,
+                               download.table,
+                               aktenzeichen,
+                               ecli,
+                               entscheidung_typ
+                               verfahrensart){
+
+
+    
+
+
+
+
+    
+    }
+
+
+    
