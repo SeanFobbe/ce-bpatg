2fee990095d914aa949a76e166ba757ce545e83a
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-25 03:18:09 +0200

    add test stuff

--- functions/f.dataset_finalize.R
+++ functions/f.dataset_finalize.R
@@ -18,4 +18,11 @@ f.dataset_finalize <- function(x,
     }
 
 
-    
+x <- tar_read(dt.bpatg.datecleaned)
+download.table <- tar_read(dt.download.final)
+
+
+verfahrensart <- tar_read(var_verfahrensart)
+entscheidung_typ <- tar_read(var_entscheidung_typ)
+aktenzeichen <- tar_read(var_aktenzeichen)
+ecli <- tar_read(var_ecli)
