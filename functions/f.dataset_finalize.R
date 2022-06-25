e857caa6147b7b61cfbdf04e4e244ab4d84cdf71
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-25 03:25:01 +0200

    finish up final function

--- functions/f.dataset_finalize.R
+++ functions/f.dataset_finalize.R
@@ -9,20 +9,38 @@ f.dataset_finalize <- function(x,
                                verfahrensart){
 
 
+    dt <- cbind(x,
+                aktenzeichen,
+                ecli,
+                entscheidung_typ,
+                verfahrensart)
     
+    
+    dt.final <- merge(dt.final,
+                      download.table[,.(doc_id,
+                                        url,
+                                        comment,
+                                        leitsatz,
+                                        berichtigung,
+                                        wirkung,
+                                        ruecknahme,
+                                        erledigung)],
+                      by = "doc_id",
+                      all = TRUE)
 
 
+    return(dt.final)
 
 
     
     }
 
 
-x <- tar_read(dt.bpatg.datecleaned)
-download.table <- tar_read(dt.download.final)
+## x <- tar_read(dt.bpatg.datecleaned)
+## download.table <- tar_read(dt.download.final)
 
 
-verfahrensart <- tar_read(var_verfahrensart)
-entscheidung_typ <- tar_read(var_entscheidung_typ)
-aktenzeichen <- tar_read(var_aktenzeichen)
-ecli <- tar_read(var_ecli)
+## verfahrensart <- tar_read(var_verfahrensart)
+## entscheidung_typ <- tar_read(var_entscheidung_typ)
+## aktenzeichen <- tar_read(var_aktenzeichen)
+## ecli <- tar_read(var_ecli)
