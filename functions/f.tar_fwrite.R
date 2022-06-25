9c5b61f9f42abc353d4134f304dfdf9f96e67eb8
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-25 18:24:38 +0200

    add tar_fwrite

---
+++ functions/f.tar_fwrite.R
@@ -0,0 +1,22 @@
+
+
+#' Implements the data.table::fwrite function for use in the targets context. Mainly necessary to return the CSV file name for later tracking.
+#'
+#' @param x The data.table to be written to disk.
+#' @param filename The filename of the CSV file.
+#'
+#' @param return The filename of the CSV file.
+
+
+
+f.tar_fwrite <- function(x,
+                         filename){
+
+
+    data.table::fwrite(x,
+                       file = filename,
+                       na = "NA")
+
+    return(filename)
+    
+    }
