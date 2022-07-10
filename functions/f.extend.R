cc913992ca0c03bfb3238d8b677ebb57f46110e8
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-10 03:44:33 +0200

    fix docs extent fun

--- functions/f.extend.R
+++ functions/f.extend.R
@@ -1,9 +1,9 @@
-
 #' Diese Funktion nimmt eine ganzzahlige y-Variable als Maximum einer Sequenz von 1 bis y und weist ihr in einer data.table jeweils immer die gleiche x-Variable zu.
 
 #' @param x Integer.
 #' @param y Integer.
-
+#'
+#' @return Data.table.
 
 f.extend <- function(x, y, begin = 0){
     y.ext <- begin:y
