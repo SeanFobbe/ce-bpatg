daec8ddecd6ba94885c9be7d77e2eb55546fd42e
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-10 17:14:25 +0200

    add verfügung to entscheidungstest

--- functions/f.var_entscheidung_typ.R
+++ functions/f.var_entscheidung_typ.R
@@ -1,8 +1,11 @@
-
-
-#' @param x Ein Vektor aus Strings ("Character"), die den Text von Entscheidungen des Bundespatentgerichts enthalten.
+#' Entscheidungstyp extrahieren
 #'
-#' @param return Ein Vektor aller Entscheidungstypen. Entweder "B" für Beschluss oder "U" für Urteil.
+#' Durchsucht den Volltext einer Gerichtsentscheidung auf das Vorhandensein der Wörter "Beschluss", "Urteil" oder "Verfügung". Der erste Treffer stellt den Typ der Entscheidung dar.
+
+
+#' @param x Character. Ein Vektor, die den Volltext von Gerichtsentscheidungen enthalten.
+#'
+#' @param return Character. Ein Vektor aller Entscheidungstypen. Entweder "B" für Beschluss oder "U" für Urteil.
 
 
 f.var_entscheidung_typ_bpatg <- function(x){
@@ -11,18 +14,27 @@ f.var_entscheidung_typ_bpatg <- function(x){
     entscheidung_typ <- stringi::stri_extract_first(str = x,
                                                     regex = paste0(gsub(" ", " *", "B E S C H L U S S"),
                                                                    "|",
-                                                                   gsub(" ", " *", "U R T E I L")),
+                                                                   gsub(" ", " *", "U R T E I L"),
+                                                                   "|",
+                                                                   gsub(" ", " *", "V E R F Ü G U N G")),
                                                     case_insensitive = TRUE)
 
     ## Auf einzelnen Buchstaben reduzieren
-    entscheidung_typ <- gsub("([BU]).+",
+    entscheidung_typ <- gsub("([BUV]).+",
                              "\\1",
                              toupper(entscheidung_typ))
 
-    ## Strenger Test
-    if (any(!entscheidung_typ %in% c("B", "U"))){
+    ## Unit Test: Begrenzung auf bestimmte Entscheidungstypen
+    if (any(!entscheidung_typ %in% c("B", "U", "V"))){
 
-        stop("Fehler: Nicht alle Entscheidungstypen sind entweder 'U' oder 'B'.")
+        stop("Nicht alle Entscheidungstypen sind 'U', 'B' oder 'V'.")
+        
+    }
+
+    ## Unit Test: Vollständigkeit der Extraktion
+    if (!length(x) == length(entscheidung_typ)){
+
+        stop("Extraktion nicht vollständig.")
         
     }
 
