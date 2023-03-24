3d64056819d6ca70ee5900227d69aeaa79297b22
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-24 14:57:22 +0100

    update aktenzeichen fun; fix single-digit issue in 2000s; add unit tests

--- functions/f.var_aktenzeichen.R
+++ functions/f.var_aktenzeichen.R
@@ -19,6 +19,14 @@ f.var_aktenzeichen <- function(x,
                                gericht){
 
 
+    ## Unit Test
+    test_that("Argumente entsprechen Erwartungen.", {
+        expect_s3_class(x, "data.table")
+        expect_s3_class(az.brd, "data.table")
+        expect_type(gericht, "character")
+    })
+
+    
     ## Datensatz auf relevante Daten reduzieren
     az.gericht <- az.brd[stelle == gericht & position == "hauptzeichen"]
 
@@ -31,7 +39,7 @@ f.var_aktenzeichen <- function(x,
                                  az.gericht$zeichen_code,
                                  az.gericht$zeichen_original),
                            " ",
-                           x$eingangsnummer,
+                           formatC(x$eingangsjahr_az, flag = "0", width = 2),
                            "/",
                            x$eingangsjahr_az)
 
@@ -40,6 +48,39 @@ f.var_aktenzeichen <- function(x,
     ## aktenzeichen <- gsub("NA ",
     ##                      "",
     ##                      aktenzeichen)
+
+
+
+    
+    ## REGEX-Validierung: Aktenzeichen
+    
+    regex.test <- grep(paste0("[0-9NA]+", # Spruchkörper
+                              " ",
+                              "[A-Za-z-]+", # Registerzeichen
+                              " ",
+                              "[0-9]+", # Eingangsnummer
+                              "\\.",
+                              "[0-9]{2}"), # Eingangsjahr
+                       aktenzeichen,
+                       value = TRUE,
+                       invert = TRUE)
+
+    ## Fehlerhafte Aktenzeichen
+
+    if(length(regex.test) != 0){
+        warning("Fehlerhafte Aktenzeichen:")
+        warning(regex.test)
+    }
+
+    ## Unit Test
+    test_that("Aktenzeichen entsprechen Erwartungen.", {
+        expect_type(aktenzeichen, "character")
+        expect_length(regex.test,  0)
+        expect_length(aktenzeichen, nrow(x))
+    })
+
+
+    
 
     return(aktenzeichen)    
     
