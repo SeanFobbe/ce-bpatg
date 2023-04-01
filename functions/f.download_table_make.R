0a84c0e9287fe9fb0574e5047ed0d537658bca68
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-04-01 22:51:54 +0200

    update download table make fun

--- functions/f.download_table_make.R
+++ functions/f.download_table_make.R
@@ -1,120 +1,48 @@
 #' Vorläufige Download-Tabelle erstellen
 
-#' Diese Funktion wertet die Entscheidungsdatenbank des Bundespatentgerichts aus und sammelt Links zu den Entscheidungsvolltexten und verbindet sie mit den in der Datenbank angegebenen Metadaten.
+#' Diese Funktion wertet die Entscheidungsdatenbank des Bundespatentgerichts aus und sammelt Links zu den Volltexten der Entscheidungen und verbindet sie mit den in der Datenbank angegebenen Metadaten.
 
-#' @param x Data.table. Der Umfang der Datenbankseiten, der berücksichtigt werden soll. Ist im Datensatz im Ordner data/ dokumentiert.
-#' @param debug.toggle Logical. Ob der Debugging-Modus aktiviert werden soll. Im Debugging-Modus wird nur eine reduzierte Anzahl Datenbankseiten ausgewertet. Jede Seite enthält idR 30 Entscheidungen.
-#' @param debug.pages Integer. Anzahl der auszuwertenden Datenbankseiten.
+#' @param files.html String. Pfade zu den HTML-Dateien der Datenbankseiten des BPatG.
 
-
-#' @return Data.table. Eine Tabelle mit allen URLs und in der Datenbank verfügbaren Metadaten.
+#' @return Data.table. Eine Tabelle mit allen URLs zu PDFs und in der Datenbank verfügbaren Metadaten.
 
 
 
+## DEBUGGING
 
-f.download_table_make <- function(x,
-                                  debug.toggle = FALSE,
-                                  debug.pages = 50){
-
-
-    ## Genauen Such-Umfang berechnen
-
-    scope <- f.extend(x$year,
-                      x$pagemax0)
+## files.html <- list.files("files/html", full.names = TRUE)
 
 
-    scope <- rbindlist(scope)
-
-    setnames(scope,
-             c("year",
-               "page"))
 
 
-    
-    ## [Debugging Modus] Reduzierung des Such-Umfangs
 
-    if (debug.toggle == TRUE){
-        scope <- scope[sample(scope[,.N], debug.pages)][order(year, page)]
-    }
 
+f.download_table_make <- function(files.html){
 
 
     ## Metadaten extrahieren
-    
-    meta.all.list <- vector("list",
-                            scope[,.N])
 
-    scope.random <- sample(scope[,.N])
+    list <- lapply(files.html,
+                   f.parse_html_bpatg)
 
-    for (i in seq_along(scope.random)){
-        
-        year <- scope$year[scope.random[i]]
-        page <- scope$page[scope.random[i]]
-
-        URL  <- paste0("https://juris.bundespatentgericht.de/cgi-bin/rechtsprechung/list.py?Gericht=bpatg&Art=en&Datum=",
-                       year,
-                       "&Seite=",
-                       page)
-        
-        html <- read_html(URL)
-        
-        url <-  html_nodes(html, "a" )%>% html_attr('href')
-        
-        url <- grep ("Blank=1.pdf",
-                      url,
-                      ignore.case = TRUE,
-                      value = TRUE)
-        
-        url <- sprintf("https://juris.bundespatentgericht.de/cgi-bin/rechtsprechung/%s",
-                        url)
-        
-
-        datum <- html_nodes(html, "[class='EDatum']") %>% html_text(trim = TRUE)
-
-        senatsgruppe <- html_nodes(html, "[class='ESpruchk']") %>% html_text(trim = TRUE)
-        
-        az <- html_nodes(html, "[class='EAz']") %>% html_text(trim = TRUE)
-
-        bemerkung <- html_nodes(html, "[class='ETitel']") %>% html_text(trim = TRUE)
-
-        meta.all.list[[scope.random[i]]] <- data.table(year,
-                                                       page,
-                                                       url,
-                                                       datum,
-                                                       senatsgruppe,
-                                                       az,
-                                                       bemerkung)
-
-        remaining <- length(scope.random) - i
-        
-        if ((remaining %% 100) == 0){
-            message(paste(Sys.time(),
-                          "| Noch",
-                          remaining,
-                          "verbleibend."))
-        }
-
-        if((i %% 100) == 0){
-            Sys.sleep(runif(1, 5, 15))
-        }else{
-            Sys.sleep(runif(1, 1.5, 2.5))
-        }    
-    }
-
-    
-    ## Zusammenfügen
-    dt.download <- rbindlist(meta.all.list)
+    dt <- rbindlist(list)
 
     
     ## Datum bereinigen
-    dt.download[, datum := {
+    dt[, datum := {
         datum <- as.character(datum)
         datum <- as.IDate(datum, "%d.%m.%Y")
         list(datum)}]
+
     
+    ## Bemerkungen bereinigen
+    
+    dt$bemerkung <- gsub("Leitsaetz|Leitsaz|Leitsazt",
+                                  "Leitsatz",
+                                  dt$bemerkung)
 
-    return(dt.download)
-
+    return(dt)
+    
 }
 
 
@@ -123,3 +51,67 @@ f.download_table_make <- function(x,
 
 
 
+
+
+
+
+
+f.parse_html_bpatg <- function(file){
+
+    
+    ## Read HTML
+    html <- rvest::read_html(file)
+
+
+    ## Extract PDF link
+    url <- rvest::html_nodes(html, "a" )
+    url <- rvest::html_attr(url, 'href')
+    
+    url <- grep ("Blank=1.pdf",
+                 url,
+                 ignore.case = TRUE,
+                 value = TRUE)
+    
+    url <- sprintf("https://juris.bundesgerichtshof.de/cgi-bin/rechtsprechung/%s",
+                   url)
+
+
+    
+    ## Extract Metadata
+    
+    datum <- rvest::html_nodes(html, "[class='EDatum']")
+    datum <- rvest::html_text(datum, trim = TRUE)
+
+    senatsgruppe <- rvest::html_nodes(html, "[class='ESpruchk']")
+    senatsgruppe <- rvest::html_text(senatsgruppe, trim = TRUE)
+    
+    az <- rvest::html_nodes(html, "[class='EAz']")
+    az <- rvest::html_text(az, trim = TRUE)
+
+    bemerkung <- rvest::html_nodes(html, "[class='ETitel']")
+    bemerkung <- rvest::html_text(bemerkung, trim = TRUE)
+
+
+    ## Extract year and page from filename
+
+    year <- gsub("([0-9]{4})-[0-9]{4}\\.html", "\\1", basename(file))
+    year <- rep(year, length(url))
+    
+    page <- gsub("[0-9]{4}-([0-9]{4})\\.html", "\\1", basename(file))
+    page <- rep(page, length(url))
+
+
+
+    ## Return Data Table
+    dt.return <- data.table(year,
+                            page,
+                            url,
+                            datum,
+                            senatsgruppe,
+                            az,
+                            bemerkung)
+
+    return(dt.return)
+    
+    
+}
