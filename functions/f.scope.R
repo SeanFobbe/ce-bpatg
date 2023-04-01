d2c206477030052dde4e3865f44c9ac0b2dfb1a6
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-04-01 17:24:17 +0200

    add scope fun

---
+++ functions/f.scope.R
@@ -0,0 +1,111 @@
+#' f.scope
+
+
+#' Maximalen Umfang der Datenbankabfrage für BPatG bestimmen.
+#' 
+#' @param debug.toggle Logical. Ob der Debugging-Modus aktiviert werden soll. Im Debugging-Modus wird nur eine reduzierte Anzahl Datenbankseiten ausgewertet. Jede Seite enthält idR 30 Entscheidungen.
+#' @param debug.pages Integer. Anzahl der auszuwertenden Datenbankseiten.
+#' 
+#' @return Data.table. Enthält die URL und den Dateinamen für alle relevanten Datenbankseiten.
+
+
+
+f.scope  <- function(debug.toggle = FALSE,
+                     debug.pages = 50){
+
+
+    year <- 2000:format(Sys.Date(), "%Y")
+
+    url.year <- paste0("https://juris.bundespatentgericht.de/cgi-bin/rechtsprechung/list.py?Gericht=bpatg&Art=en&Datum=",
+                       year)
+
+
+
+    url.all <- lapply(url.year, f.linkextract)
+
+    pagemax0 <- unlist(lapply(url.all, f.findmax))
+    
+    scope <- f.extend(year, pagemax0)
+    scope <- rbindlist(scope)
+    
+    setnames(scope,
+             c("year",
+               "page"))
+
+
+    
+    ## [Debugging Modus] Reduzierung des Such-Umfangs
+
+    if (debug.toggle == TRUE){
+        scope <- scope[sample(scope[,.N], debug.pages)][order(year, page)]
+    }
+
+
+
+
+    url  <- paste0("https://juris.bundespatentgericht.de/cgi-bin/rechtsprechung/list.py?Gericht=bpatg&Art=en&Datum=",
+                   scope$year,
+                   "&Seite=",
+                   scope$page,
+                   "&Sort=1")
+
+
+    filename <- paste0(scope$year,
+                       "-",
+                       formatC(scope$page,
+                               width = 4,
+                               flag = 0),
+                       ".html")
+    
+
+
+    dt.return <- data.table(url,
+                            filename)
+
+
+    
+    return(dt.return)
+    
+
+}
+
+
+
+
+
+
+#' @param x Character. Ein Vektor aus URLs der BPatG-Datenbank für ein bestimmtes Jahr.
+
+#' @return Die maximale Datenbankseite für ein bestimmtes Jahr.
+
+
+
+f.findmax <- function(x){
+
+    url.relevant <- grep("Seite=[0-9]+", x, value = TRUE)
+    max <- max(as.integer(gsub(".*Seite\\=([0-9]+).*", "\\1", url.relevant)))
+    
+    return(max)
+
+    
+}
+
+
+
+
+    
+
+
+
+#' Diese Funktion nimmt eine ganzzahlige y-Variable als Maximum einer Sequenz von 1 bis y und weist ihr in einer data.table jeweils immer die gleiche x-Variable zu.
+
+f.extend <- function(x, y, begin = 0){
+    y.ext <- begin:y
+    x.ext <- rep(x, length(y.ext))
+    dt.out <- list(data.table(x.ext, y.ext))
+    return(dt.out)
+}
+
+f.extend <- Vectorize(f.extend)
+
+
