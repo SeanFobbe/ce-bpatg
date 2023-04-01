ae969ac9028d611904257db2b9e2e94eab7c7187
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-04-01 23:00:16 +0200

    remvoe old run script

--- run_dataset.R
+++
@@ -1,9 +0,0 @@
-# Run full data set pipeline
-
-dir.create("output", showWarnings = FALSE)
-
-rmarkdown::render("CE-BPatG_Compilation.Rmd",
-                  output_file = paste0("output/CE-BPatG_",
-                                       Sys.Date(),
-                                       "_CompilationReport.pdf"))
-
